f8946d1d7d38082a7738224a574527d14374f790
Eddy Kim <eddy.kim@ibm.com>
2022-11-01 16:26:52 +0800

    Update pr_jenkins_job.py

--- pr_jenkins_job.py
+++ pr_jenkins_job.py
@@ -190,6 +190,9 @@ class JobRunner:
     required=True,
 )
 def driver(apikey, project_id, job_name):  # sourcery skip: raise-specific-error
+    print(apikey)
+    print(project_id)
+    print(job_name)
     jobrun = JobRunner(apikey, project_id)
     job_id = jobrun.retrieve_job_id(name=job_name)
     print(f" Job ID is -->{job_id}")
